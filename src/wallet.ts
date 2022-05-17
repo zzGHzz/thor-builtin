b580e460d04cb66f32eabd1b66e3001c5984b5b7
zzGHzz <zzGHzz@github>
2022-05-17 13:20:33 +0800

    add solo default account and hd wallet path

---
+++ src/wallet.ts
@@ -0,0 +1,19 @@
+import { HDNode } from 'thor-devkit'
+
+const word = 'denial kitchen pet squirrel other broom bar gas better priority spoil cross'
+const hdNode = HDNode.fromMnemonic(word.split(' '))
+
+let defaultAccounts: string[] = []
+for (let i = 0; i < 10; i++) {
+	const priv = hdNode.derive(i).privateKey
+	defaultAccounts.push('0x' + priv?.toString('hex'))
+}
+
+const SoloDefault = {
+	mnemonic: word,
+	accounts: defaultAccounts
+}
+
+const HDWalletDerivationPath =  `m/44'/818'/0'/0`
+
+export { SoloDefault, HDWalletDerivationPath }
