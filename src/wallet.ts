50341fc5a7a0b122ff292a3d148a4e4251e5f932
zzGHzz <zzGHzz@github>
2022-05-18 11:01:02 +0800

    add default account addresses for solo network

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,17 +1,20 @@
-import { HDNode } from 'thor-devkit'
+import { HDNode, address } from 'thor-devkit'
 
 const word = 'denial kitchen pet squirrel other broom bar gas better priority spoil cross'
 const hdNode = HDNode.fromMnemonic(word.split(' '))
 
-let defaultAccounts: string[] = []
+let privKeys: string[] = []
+let accounts: string[] = []
 for (let i = 0; i < 10; i++) {
 	const priv = hdNode.derive(i).privateKey
-	defaultAccounts.push('0x' + priv?.toString('hex'))
+	privKeys.push('0x' + priv?.toString('hex'))
+	accounts.push(address.fromPublicKey(<Buffer>priv))
 }
 
 const SoloDefault = {
 	mnemonic: word,
-	accounts: defaultAccounts
+	privKeys: privKeys,
+	accounts: accounts
 }
 
 const HDWalletDerivationPath =  `m/44'/818'/0'/0`
